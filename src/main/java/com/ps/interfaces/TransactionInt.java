4c273e72100aa88caccee5bda444196875e14f61
Marshall <mellisgibbs@gmail.com>
2024-07-09 16:13:59 -0400

    added methods

--- src/main/java/com/ps/interfaces/TransactionInt.java
+++ src/main/java/com/ps/interfaces/TransactionInt.java
@@ -1,4 +1,15 @@
 package com.ps.interfaces;
 
+import com.ps.Transaction;
+
+import java.util.List;
+
 public interface TransactionInt {
+    List<Transaction> getAllTransactions();
+    List<Transaction> getAllPayments();
+    List<Transaction> getAllDeposits();
+    Transaction getOneTransaction(int id);
+    int createTransaction(Transaction transaction);
+    void updateTransaction(int id, Transaction transaction);
+    void deleteTransaction(int id);
 }
