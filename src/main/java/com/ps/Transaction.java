8ae0735ca387721172ff749d62090e03f6c7c415
Marshall <mellisgibbs@gmail.com>
2024-07-15 09:55:44 -0400

    updated 'Transaction' class

--- src/main/java/com/ps/Transaction.java
+++ src/main/java/com/ps/Transaction.java
@@ -1,92 +1,87 @@
 package com.ps;
 
 
-import java.time.LocalDate;
-
-import java.time.LocalTime;
-
-
 public class Transaction {
     private int id;
-    private LocalDate date; // Make this "String date"
-    private LocalTime time; // Remove this
+    private String date;
     private String description;
     private String vendor;
     private float amount;
     
-    // Add a new constructor
-    
-    public Transaction(int id, LocalDate date, LocalTime time, String description, String vendor, float amount) {
+    public Transaction(int id, String date, String description, String vendor, float amount)
+    {
         this.id = id;
         this.date = date;
-        this.time = time;
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
     }
     
-    public Transaction(LocalDate date, LocalTime time, String description, String vendor, float amount) {
+    public Transaction(String date, String description, String vendor, float amount)
+    {
+        this.id = 0;
         this.date = date;
-        this.time = time;
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
     }
     
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
     
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
     
-    public LocalDate getDate() {
+    public String getDate()
+    {
         return date;
     }
-
-    public void setDate(LocalDate date) {
+    
+    public void setDate(String date)
+    {
         this.date = date;
     }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-    public String getDescription() {
+    
+    public String getDescription()
+    {
         return description;
     }
-
-    public void setDescription(String description) {
+    
+    public void setDescription(String description)
+    {
         this.description = description;
     }
-
-    public String getVendor() {
+    
+    public String getVendor()
+    {
         return vendor;
     }
-
-    public void setVendor(String vendor) {
+    
+    public void setVendor(String vendor)
+    {
         this.vendor = vendor;
     }
-
-    public float getAmount() {
+    
+    public float getAmount()
+    {
         return amount;
     }
-
-    public void setAmount(float amount) {
+    
+    public void setAmount(float amount)
+    {
         this.amount = amount;
     }
     
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Transaction{" +
                 "id=" + id +
-                ", date=" + date +
-                ", time=" + time +
+                ", date='" + date + '\'' +
                 ", description='" + description + '\'' +
                 ", vendor='" + vendor + '\'' +
                 ", amount=" + amount +
