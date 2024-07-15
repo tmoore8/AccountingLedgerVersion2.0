3ac684d6ed707f2ea2324f99b5fbe9a3b69699c3
Marshall <mellisgibbs@gmail.com>
2024-07-14 21:28:14 -0400

    finished 'getAllPayments()'

--- src/main/java/com/ps/DAOs/TransactionDao.java
+++ src/main/java/com/ps/DAOs/TransactionDao.java
@@ -11,30 +11,37 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionDao implements TransactionInt {
+public class TransactionDao implements TransactionInt
+{
     private BasicDataSource basicDataSource;
     
-    public TransactionDao(BasicDataSource basicDataSource) {
+    public TransactionDao(BasicDataSource basicDataSource)
+    {
         this.basicDataSource = basicDataSource;
     }
     
     @Override
-    public List<Transaction> getAllTransactions() {
+    public List<Transaction> getAllTransactions()
+    {
         List<Transaction> transactions = new ArrayList<>();
         
         String sql = "SELECT * FROM transactions";
         
-        try (
+        try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql);
                 ResultSet row = statement.executeQuery();
-        ) {
-           while (row.next()) {
-               Transaction transaction = mapRow(row);
-               
-               transactions.add(transaction);
-           }
-        } catch (SQLException e) {
+        )
+        {
+            while(row.next())
+            {
+                Transaction transaction = mapRow(row);
+                
+                transactions.add(transaction);
+            }
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
         
@@ -42,36 +49,67 @@ public class TransactionDao implements TransactionInt {
     }
     
     @Override
-    public List<Transaction> getAllPayments() {
+    public List<Transaction> getAllPayments()
+    {
+        List<Transaction> payments = new ArrayList<>();
+        
+        String sql = "SELECT * " +
+                " FROM payments AS p " +
+                " JOIN transactions AS t " +
+                "   ON p.transaction_id = t.transaction_id";
+        
+        try(
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet row = statement.executeQuery();
+                )
+        {
+            while (row.next()) {
+                Transaction transaction = mapRow(row);
+                
+                payments.add(transaction);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return payments;
+    }
+    
+    @Override
+    public List<Transaction> getAllDeposits()
+    {
         return null;
     }
     
     @Override
-    public List<Transaction> getAllDeposits() {
+    public Transaction getOneTransaction(int id)
+    {
         return null;
     }
     
     @Override
-    public Transaction getOneTransaction(int id) {
-        return null;
-    }
-    
-    @Override
-    public int createTransaction(Transaction transaction) {
+    public int createTransaction(Transaction transaction)
+    {
         return 0;
     }
     
     @Override
-    public void updateTransaction(int id, Transaction transaction) {
+    public void updateTransaction(int id, Transaction transaction)
+    {
     
     }
     
     @Override
-    public void deleteTransaction(int id) {
+    public void deleteTransaction(int id)
+    {
     
     }
     
-    private Transaction mapRow(ResultSet row) throws SQLException {
+    private Transaction mapRow(ResultSet row) throws SQLException
+    {
         int    id          = row.getInt("transaction_id");
         String date        = row.getTimestamp("date").toString();
         String description = row.getString("description");
