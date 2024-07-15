82af97af1fb58b2444e378c8047562a5a5b3d133
Alisha Yu <alisha.yu15@gmail.com>
2024-07-15 11:45:25 -0400

    Updated Main class, README, + transactions.txt

---
+++ README.md
@@ -0,0 +1,180 @@
+# Accounting Ledger
+
+## Table of Contents
+- [Overview]()
+- [Main Menu]()
+- [Ledger Menu]()
+- [Reports Menu]()
+- [Interesting Pieces of Code]()
+- [Contributors]()
+
+## Overview
+
+The Accounting Ledger System is a command-line Java application designed to manage transactions and ledger entries for a small business or personal finance tracking. It allows users to add deposits, make payments, view the ledger, and generate reports based on different criteria such as date range or vendor.
+
+## Features
+
+### Main Menu
+
+#### The Main Menu Screen is where users are prompted to:
+- Make a deposit (which is reflected as a credit to their accounts)
+- Make a payment (which is reflected as a debit to their accounts)
+- View their accounts ledger
+- Access and manage their financial goals
+- Exit the application
+
+><details>
+><summary> Main Menu </summary>
+>
+> ![img.png](src/main/java/com/ps/images/img.png)
+></details>
+
+#### Adding a deposit
+
+><details>
+><summary> Deposits </summary>
+>
+> ![img_2.png](src/main/java/com/ps/images/img_2.png)
+></details>
+
+#### Payments
+
+><details>
+><summary> Making a Payment </summary>
+>
+>![img_3.png](src/main/java/com/ps/images/img_3.png)
+></details>
+
+#### Ledger Screen
+
+><details>
+><summary> Accessing the Ledger Screen </summary>
+>
+> ![img_4.png](src/main/java/com/ps/images/img_4.png)
+></details>
+
+#### Exit Screen
+
+><details>
+><summary> Exiting the Application </summary>
+>
+> ![img_5.png](src/main/java/com/ps/images/img_5.png)
+></details>
+
+[Back to the Top]()
+
+### Ledger Menu
+From the Ledger Menu all transactions can be displayed or they can be sorted into deposits (+) or payments (-) or the report menu can be opened
+
+><details>
+><summary> All Transactions </summary>
+>
+> ![img_6.png](src/main/java/com/ps/images/img_6.png)
+></details>
+
+><details>
+><summary> Deposits </summary>
+>
+> ![img_7.png](src/main/java/com/ps/images/img_7.png)
+></details>
+
+><details>
+><summary> Payments </summary>
+>
+> ![img_8.png](src/main/java/com/ps/images/img_8.png)
+></details>
+
+[Back to the Top]()
+
+### Reports Menu
+From the Reports menu you can display reports containing all transactions within a given time period or search by vendor
+
+><details>
+><summary> Reports Menu </summary>
+>
+> ![img_9.png](src/main/java/com/ps/images/img_9.png)
+></details>
+
+><details>
+><summary> Month to Date </summary>
+>
+> ![img_10.png](src/main/java/com/ps/images/img_10.png)
+></details>
+
+><details>
+><summary> Search by Vendor </summary>
+>
+> ![img_11.png](src/main/java/com/ps/images/img_11.png)
+></details>
+
+><details>
+><summary> Custom Search </summary>
+>
+> Image here
+></details>
+
+[Back to the Top]()
+
+## Interesting Pieces of Code
+
+### Hibba's Code
+
+### Marshall's Code
+
+### Tristan's Code
+One piece of code I found interesting was for my menus. Instead of using nest do while loops with switches I created methods containing the submenus to keep the code more concise and readable.
+
+><details>
+><summary> Main Menu Code </summary>
+>
+> ![img_12.png](src/main/java/com/ps/images/img_12.png)
+></details>
+
+><details>
+><summary> Ledger Menu Code </summary>
+>
+> ![img_13.png](src/main/java/com/ps/images/img_13.png)
+></details>
+
+><details>
+><summary> Reports Menu Code </summary>
+>
+> ![img_14.png](src/main/java/com/ps/images/img_14.png)
+></details>
+
+### Alisha's Interesting Piece of Code
+
+If I had to pick a piece of code that I found interesting, I would definitely have to pick where I wrote a  helper method 
+which helped me display all the transactions throughout the application without needing to format and print out every time 
+I need to show the user their requested transactions.
+
+><details>
+><summary> Displaying Transactions Helper Method </summary>
+>
+> Image here
+></details>
+
+[Back to the Top]()
+
+## Contributors
+
+### Hibba Afzal
+
+- [Hibba's Github](https://github.com/hibbaafzal)
+
+### Marshall Ellis-Gibbs
+
+- [Marshall's Github](https://github.com/MarshallE-G)
+
+### Tristan Moorehead 
+
+- [Tristan's Github](https://github.com/tmoore8)
+
+### Alisha Yu
+
+- [Alisha's Github](https://github.com/alyu15)
+
+[Back to the Top]()
+
+
+
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -4,7 +4,10 @@ import com.ps.DAOs.TransactionDao;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,28 +16,29 @@ public class Main {
     public static void main(String[] args) {
         display(args);
     }
-
-    public static void init(String[] args) {
+    public static void init(String[] args){
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl("jdbc:mysql://localhost:3306/AccountingLedger");
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         basicDataSource.setUsername(args[0]);
         basicDataSource.setPassword(args[1]);
-        transactionDao = new TransactionDao(basicDataSource);
-    }
 
-    public static void display(String[] args) {
+        transactionDao = new TransactionDao(basicDataSource);
+
+    }
+    public static void display(String[] args){
         init(args);
         Scanner scanner = new Scanner(System.in);
         Ledger ledger = new Ledger();
         int userIn;
         do {
-            System.out.println("Welcome to HAMM Accounting. Please select an option: ");
-            System.out.println("\t1) Add deposit");
+            System.out.println("\nWelcome to HAMM Accounting. Please select an option: ");
+            System.out.println("\n\t1) Add deposit");
             System.out.println("\t2) Make Payment");
             System.out.println("\t3) Display Ledger");
             System.out.println("\t4) Exit");
             userIn = scanner.nextInt();
+
             switch (userIn) {
                 case 1:
                     //Add a deposit prompt the user for a deposit information and save it to CSV
@@ -57,13 +61,14 @@ public class Main {
         } while (userIn != 4);
     }
 
+
     //method to display ledger menu
     private static void displayLedgerMenu(Scanner scanner, Ledger ledger) {
-        System.out.println("Ledger: ");
+        System.out.println("\nLedger: ");
         int ledgerCommand;
         do {
-            System.out.println("\t1) Display all entries");
-            System.out.println("\t2) Display deposits");
+            System.out.println("\n\t1) Display all entries: ");
+            System.out.println("\t2) Display deposits;");
             System.out.println("\t3) Display Payments");
             System.out.println("\t4) Reports Menu");
             System.out.println("\t5) Back");
@@ -72,15 +77,18 @@ public class Main {
             switch (ledgerCommand) {
                 case 1:
                     //display all entries
-                    ledger.displayAllTransactions();
+                    ArrayList<Transaction> allTransactions = ledger.displayAllTransactions();
+                    displayTransactions(allTransactions);
                     break;
                 case 2:
                     // display deposits
-                    ledger.displayOnlyDeposits();
+                    ArrayList<Transaction> onlyDepositsTransactions = ledger.displayOnlyDeposits();
+                    displayTransactions(onlyDepositsTransactions);
                     break;
                 case 3:
                     //display payments
-                    ledger.displayOnlyPayments();
+                    ArrayList<Transaction> onlyPaymentsTransactions = ledger.displayOnlyPayments();
+                    displayTransactions(onlyPaymentsTransactions);
                     break;
                 case 4:
                     //reports menu
@@ -90,86 +98,148 @@ public class Main {
                     break;
                 default:
                     System.out.println("Invalid option.");
+
             }
         } while (ledgerCommand != 5);
+
     }
 
     //method to display reports menu
     private static void displayReportsMenu(Scanner scanner, Ledger ledger) {
         int reportCommand;
         do {
-            System.out.println("Search Reports by: ");
-            System.out.println("\t1) Month to Date");
-            System.out.println("\t2) Previous Month");
+            System.out.println("\nSearch Reports by: ");
+            System.out.println("\n\t1) Month to Date: ");
+            System.out.println("\t2) Previous Month;");
             System.out.println("\t3) Year to Date");
             System.out.println("\t4) Previous Year");
             System.out.println("\t5) Vendor");
-            System.out.println("\t6) Back");
-
+            System.out.println("\t6) Custom Search");
+            System.out.println("\t7) Back");
             reportCommand = scanner.nextInt();
             scanner.nextLine();//consume new line;
+
             switch (reportCommand) {
                 case 1:
                     //month to date
                     LocalDate currentDate = LocalDate.now();
                     LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
                     LocalDate lastDayOfMonth = currentDate.withDayOfMonth(currentDate.lengthOfMonth());
+
                     ArrayList<Transaction> monthToDateTransactions = ledger.filterTransactionsByDateRange(firstDayOfMonth, lastDayOfMonth);
-                    System.out.println("Month to Date Transactions:");
-                    for (Transaction transaction : monthToDateTransactions) {
-                        System.out.println(transaction);
-                    }
+
+                    displayTransactions(monthToDateTransactions);
                     break;
                 case 2:
                     // previous month
                     LocalDate previousMonthFirstDay = LocalDate.now().minusMonths(1).withDayOfMonth(1);
                     LocalDate previousMonthLastDay = previousMonthFirstDay.withDayOfMonth(previousMonthFirstDay.lengthOfMonth());
+
                     ArrayList<Transaction> previousMonthTransactions = ledger.filterTransactionsByDateRange(previousMonthFirstDay, previousMonthLastDay);
-                    System.out.println("Previous Month Transactions:");
-                    for (Transaction transaction : previousMonthTransactions) {
-                        System.out.println(transaction);
-                    }
+
+                    displayTransactions(previousMonthTransactions);
                     break;
                 case 3:
                     //year to date
                     LocalDate currentYearFirstDay = LocalDate.now().withDayOfYear(1);
                     LocalDate currentYearLastDay = LocalDate.now().plusYears(1).withDayOfYear(1).minusDays(1);
+
                     ArrayList<Transaction> yearToDateTransactions = ledger.filterTransactionsByDateRange(currentYearFirstDay, currentYearLastDay);
-                    System.out.println("Year to Date Transactions:");
-                    for (Transaction transaction : yearToDateTransactions) {
-                        System.out.println(transaction);
-                    }
+
+                    displayTransactions(yearToDateTransactions);
                     break;
                 case 4:
                     LocalDate previousYearFirstDay = LocalDate.now().minusYears(1).withDayOfYear(1);
                     LocalDate previousYearLastDay = LocalDate.now().withDayOfYear(1).minusDays(1);
+
                     ArrayList<Transaction> previousYearTransactions = ledger.filterTransactionsByDateRange(previousYearFirstDay, previousYearLastDay);
-                    System.out.println("Previous Year Transactions:");
-                    for (Transaction transaction : previousYearTransactions) {
-                        System.out.println(transaction);
-                    }
+
+                    displayTransactions(previousYearTransactions);
                     //previous year
                     break;
                 case 5:
-                    System.out.print("Enter vendor name: ");
+                    System.out.println("Enter vendor name: ");
+
                     String vendorName = scanner.nextLine();
                     ArrayList<Transaction> vendorTransactions = ledger.searchByVendor(vendorName);
                     if (vendorTransactions.isEmpty()) {
                         System.out.println("No transactions for vendor: " + vendorName);
                     } else {
-                        System.out.println("Transactions for vendor " + vendorName + ": ");
-                        for (Transaction transaction : vendorTransactions) {
-                            System.out.println(transaction);
-                        }
+                        displayTransactions(vendorTransactions);
                     }
                     break;
                 case 6:
 
+                        System.out.println("Enter start date (yyyy-MM-dd): ");
+                        String startDateInput = scanner.nextLine().trim();
+
+                        System.out.println("Enter end date (yyyy-MM-dd): ");
+                        String endDateInput = scanner.nextLine().trim();
+
+                            if(!startDateInput.isEmpty() && endDateInput.isEmpty()) {
+
+                                LocalDate startDate = LocalDate.parse(startDateInput);
+                                ArrayList<Transaction> startDateList = ledger.searchByOneDate(startDate);
+
+                                displayTransactions(startDateList);
+                                break;
+
+                            } else if(startDateInput.isEmpty() && !endDateInput.isEmpty()) {
+
+                                LocalDate endDate = LocalDate.parse(endDateInput);
+                                ArrayList<Transaction> endDateList = ledger.searchByOneDate(endDate);
+
+                                displayTransactions(endDateList);
+                                break;
+
+                            } else if (!startDateInput.isEmpty() && !endDateInput.isEmpty()) {
+
+                                LocalDate startDate = LocalDate.parse(startDateInput);
+                                LocalDate endDate = LocalDate.parse(endDateInput);
+                                ArrayList<Transaction> bothDatesList = ledger.searchByDates(startDate, endDate);
+
+                                displayTransactions(bothDatesList);
+                                break;
+                            }
+
+                        System.out.println("Enter description: ");
+                        String descriptionCustomSearch = scanner.nextLine().trim();
+
+                            if (!descriptionCustomSearch.isEmpty()) {
+
+                                ArrayList<Transaction> descriptionList = ledger.searchByDescription(descriptionCustomSearch);
+                                displayTransactions(descriptionList);
+                                break;
+                            }
+
+                        System.out.println("Enter vendor name: ");
+                        String vendorCustomSearch = scanner.nextLine().trim();
+
+                            if (!vendorCustomSearch.isEmpty()) {
+
+                                ArrayList<Transaction> vendorList = ledger.searchByVendor(vendorCustomSearch);
+                                displayTransactions(vendorList);
+                                break;
+                            }
+
+                        System.out.println("Enter amount: ");
+                        Float amountCustomSearch = scanner.nextFloat();
+                        scanner.nextLine();
+                            if (amountCustomSearch != null) {
+
+                                ArrayList<Transaction> amountList = ledger.searchByAmount(amountCustomSearch);
+                                displayTransactions(amountList);
+                                break;
+                            }
+                    break;
+
+                case 7:
                     break;
                 default:
                     System.out.println("Invalid option.");
+
             }
-        } while (reportCommand != 6);
+        } while (reportCommand != 7);
     }
 
     // method to receive deposit inputs
@@ -201,13 +271,11 @@ public class Main {
         }
     }
 
-
     //method to receive payment inputs
-
     private static String[] makePayment(Scanner scanner) {
 
-        boolean isPayment = false; // use for transactionDao.create()
-        boolean isDeposit = true; // use for transactionDao.create()
+        boolean isPayment = true; // use for transactionDao.create()
+        boolean isDeposit = false; // use for transactionDao.create()
 
         System.out.print("Enter the description of your payment: ");
         String description = scanner.nextLine();
@@ -231,6 +299,41 @@ public class Main {
         }
     }
 
+    public static void displayTransactions(List<Transaction> transactions) {
 
+        System.out.println("\n****************************************  Transactions  *********************************************");
+        System.out.println("Date           Time          Description                    Vendor               Amount");
+        System.out.println("------------------------------------------------------------------------------------------------------");
+
+        for(Transaction transaction: transactions) {
+
+            String transactionDate = transaction.getDate().toString();
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String formattedDate = transactionDate.formatted(dateFormatter);
+
+            System.out.printf("~ %-12s %-30s %-20s %-10.2f\n",
+
+                    formattedDate,
+                    transaction.getDescription(),
+                    transaction.getVendor(),
+                    transaction.getAmount()
+
+            );
+        }
+
+        if(transactions.isEmpty()) {
+            System.out.println("\n No transactions founds");
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
 
