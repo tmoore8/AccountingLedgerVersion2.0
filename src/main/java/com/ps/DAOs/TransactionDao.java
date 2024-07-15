e1dc2e0ddd02a5828fa635b8c75a25bbc700391c
Marshall <mellisgibbs@gmail.com>
2024-07-15 17:32:18 -0400

    created 'monthToDate()' and reformatted class

--- src/main/java/com/ps/DAOs/TransactionDao.java
+++ src/main/java/com/ps/DAOs/TransactionDao.java
@@ -8,18 +8,15 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionDao implements TransactionInt
-{
+public class TransactionDao implements TransactionInt {
     private BasicDataSource basicDataSource;
     
-    public TransactionDao(BasicDataSource basicDataSource)
-    {
+    public TransactionDao(BasicDataSource basicDataSource) {
         this.basicDataSource = basicDataSource;
     }
     
     @Override
-    public List<Transaction> getAllTransactions()
-    {
+    public List<Transaction> getAllTransactions() {
         List<Transaction> transactions = new ArrayList<>();
         
         String sql = "SELECT * FROM transactions";
@@ -28,17 +25,14 @@ public class TransactionDao implements TransactionInt
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql);
                 ResultSet row = ps.executeQuery();
-        )
-        {
-            while(row.next())
-            {
+        ) {
+            while(row.next()) {
                 Transaction transaction = mapRow(row);
                 
                 transactions.add(transaction);
             }
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
@@ -46,8 +40,7 @@ public class TransactionDao implements TransactionInt
     }
     
     @Override
-    public List<Transaction> getAllPayments()
-    {
+    public List<Transaction> getAllPayments() {
         List<Transaction> payments = new ArrayList<>();
         
         String sql = "SELECT p.*, date, description, vendor, amount " +
@@ -59,17 +52,14 @@ public class TransactionDao implements TransactionInt
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql);
                 ResultSet row = ps.executeQuery();
-        )
-        {
-            while(row.next())
-            {
+        ) {
+            while(row.next()) {
                 Transaction transaction = mapRow(row);
                 
                 payments.add(transaction);
             }
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
@@ -77,8 +67,7 @@ public class TransactionDao implements TransactionInt
     }
     
     @Override
-    public List<Transaction> getAllDeposits()
-    {
+    public List<Transaction> getAllDeposits() {
         List<Transaction> deposits = new ArrayList<>();
         
         String sql = "SELECT d.*, date, description, vendor, amount " +
@@ -90,17 +79,14 @@ public class TransactionDao implements TransactionInt
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql);
                 ResultSet row = ps.executeQuery();
-        )
-        {
-            while(row.next())
-            {
+        ) {
+            while(row.next()) {
                 Transaction transaction = mapRow(row);
                 
                 deposits.add(transaction);
             }
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
@@ -108,8 +94,7 @@ public class TransactionDao implements TransactionInt
     }
     
     @Override
-    public Transaction getTransactionById(int id)
-    {
+    public Transaction getTransactionById(int id) {
         String sql = "SELECT * " +
                 " FROM transactions " +
                 " WHERE transaction_id = ?";
@@ -117,20 +102,16 @@ public class TransactionDao implements TransactionInt
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql)
-        )
-        {
+        ) {
             ps.setInt(1, id);
             
-            try(ResultSet row = ps.executeQuery();)
-            {
-                if(row.next())
-                {
+            try(ResultSet row = ps.executeQuery();) {
+                if(row.next()) {
                     return mapRow(row);
                 }
             }
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
@@ -138,8 +119,7 @@ public class TransactionDao implements TransactionInt
     }
     
     @Override
-    public Transaction create(Transaction transaction, boolean isPayment, boolean isDeposit)
-    {
+    public Transaction create(Transaction transaction, boolean isPayment, boolean isDeposit) {
         Timestamp date = java.sql.Timestamp.valueOf(transaction.getDate());
         
         String transactionSql = "INSERT INTO transactions (date, description, vendor, amount) " +
@@ -156,8 +136,7 @@ public class TransactionDao implements TransactionInt
                 PreparedStatement transactionPs = connection.prepareStatement(transactionSql, PreparedStatement.RETURN_GENERATED_KEYS);
                 PreparedStatement paymentPs = connection.prepareStatement(paymentSql);
                 PreparedStatement depositPs = connection.prepareStatement(depositSql);
-        )
-        {
+        ) {
             transactionPs.setTimestamp(1, date);
             transactionPs.setString(2, transaction.getDescription());
             transactionPs.setString(3, transaction.getVendor());
@@ -165,12 +144,9 @@ public class TransactionDao implements TransactionInt
             
             int rowsAffected = transactionPs.executeUpdate();
             
-            if(rowsAffected > 0)
-            {
-                try(ResultSet generatedKeys = transactionPs.getGeneratedKeys();)
-                {
-                    if(generatedKeys.next())
-                    {
+            if(rowsAffected > 0) {
+                try(ResultSet generatedKeys = transactionPs.getGeneratedKeys();) {
+                    if(generatedKeys.next()) {
                         // get id of the newly inserted transaction
                         int transactionId = generatedKeys.getInt(1);
                         
@@ -178,12 +154,10 @@ public class TransactionDao implements TransactionInt
                         transaction.setId(transactionId);
                         
                         // add newly created transaction to either the Payments or Deposits table
-                        if(isDeposit)
-                        {
+                        if(isDeposit) {
                             depositPs.setInt(1, transactionId);
                             depositPs.executeUpdate();
-                        } else if(isPayment)
-                        {
+                        } else if(isPayment) {
                             paymentPs.setInt(1, transactionId);
                             paymentPs.executeUpdate();
                         }
@@ -194,8 +168,7 @@ public class TransactionDao implements TransactionInt
                 }
             }
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
@@ -203,8 +176,7 @@ public class TransactionDao implements TransactionInt
     }
     
     @Override
-    public Transaction update(int id, Transaction transaction)
-    {
+    public Transaction update(int id, Transaction transaction) {
         Timestamp date = java.sql.Timestamp.valueOf(transaction.getDate());
         
         String sql = "UPDATE transactions" +
@@ -217,8 +189,7 @@ public class TransactionDao implements TransactionInt
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement ps = connection.prepareStatement(sql);
-        )
-        {
+        ) {
             ps.setTimestamp(1, date);
             ps.setString(2, transaction.getDescription());
             ps.setString(3, transaction.getVendor());
@@ -229,17 +200,15 @@ public class TransactionDao implements TransactionInt
             
             return getTransactionById(id);
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
-        return  null;
+        return null;
     }
     
     @Override
-    public Transaction delete(int id)
-    {
+    public Transaction delete(int id) {
         String transactionSql = "DELETE FROM transactions " +
                 " WHERE transaction_id = ?;";
         
@@ -254,168 +223,201 @@ public class TransactionDao implements TransactionInt
                 PreparedStatement transactionPs = connection.prepareStatement(transactionSql);
                 PreparedStatement paymentPs = connection.prepareStatement(paymentSql);
                 PreparedStatement depositPs = connection.prepareStatement(depositSql);
-        )
-        {
+        ) {
             // delete from transactions table
             transactionPs.setInt(1, id);
             transactionPs.executeUpdate();
-    
+            
             // delete from payments table
             paymentPs.setInt(1, id);
             paymentPs.executeUpdate();
-    
+            
             // delete from deposits table
             depositPs.setInt(1, id);
             depositPs.executeUpdate();
             
             return getTransactionById(id);
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             e.printStackTrace();
         }
         
         return null;
     }
-
+    
     public List<Transaction> searchByOneDate(String date) {
-
+        
         List<Transaction> transactions = new ArrayList<>();
-
+        
         String dateQuery = "SELECT * FROM transactions WHERE date LIKE '%?%' ORDER BY date DESC;";
-
+        
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(dateQuery)
         ) {
             preparedStatement.setString(1, date);
-
+            
             try(ResultSet row = preparedStatement.executeQuery()) {
                 while(row.next()) {
                     Transaction transaction = mapRow(row);
-
+                    
                     transactions.add(transaction);
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return transactions;
-
+        
     }
-
+    
     public List<Transaction> searchByDates(String startDate, String endDate) {
-
+        
         List<Transaction> transactions = new ArrayList<>();
-
+        
         String dateQuery = "SELECT * FROM transactions WHERE date BETWEEN '?' AND '?' ORDER BY date DESC;";
-
+        
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(dateQuery)
         ) {
             preparedStatement.setString(1, startDate);
             preparedStatement.setString(2, endDate);
-
+            
             try(ResultSet row = preparedStatement.executeQuery()) {
                 while(row.next()) {
                     Transaction transaction = mapRow(row);
-
+                    
                     transactions.add(transaction);
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return transactions;
-
+        
     }
-
+    
     public List<Transaction> searchByDescription(String description) {
-
+        
         List<Transaction> transactions = new ArrayList<>();
-
+        
         String dateQuery = "SELECT * FROM transactions WHERE description LIKE '?' ORDER BY date DESC;";
-
+        
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(dateQuery)
         ) {
             preparedStatement.setString(1, description);
-
+            
             try(ResultSet row = preparedStatement.executeQuery()) {
                 while(row.next()) {
                     Transaction transaction = mapRow(row);
-
+                    
                     transactions.add(transaction);
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return transactions;
-
+        
     }
-
+    
     public List<Transaction> searchByVendor(String vendor) {
-
+        
         List<Transaction> transactions = new ArrayList<>();
-
+        
         String dateQuery = "SELECT * FROM transactions WHERE vendor LIKE '?' ORDER BY date DESC;";
-
+        
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(dateQuery)
         ) {
             preparedStatement.setString(1, vendor);
-
+            
             try(ResultSet row = preparedStatement.executeQuery()) {
                 while(row.next()) {
                     Transaction transaction = mapRow(row);
-
+                    
                     transactions.add(transaction);
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return transactions;
-
+        
     }
-
+    
     public List<Transaction> searchByAmount(float amount) {
-
+        
         List<Transaction> transactions = new ArrayList<>();
-
+        
         String dateQuery = "SELECT * FROM transactions WHERE amount = ? ORDER BY date DESC;";
-
+        
         try(
                 Connection connection = basicDataSource.getConnection();
                 PreparedStatement preparedStatement = connection.prepareStatement(dateQuery)
         ) {
             preparedStatement.setFloat(1, amount);
-
+            
             try(ResultSet row = preparedStatement.executeQuery()) {
                 while(row.next()) {
                     Transaction transaction = mapRow(row);
-
+                    
                     transactions.add(transaction);
                 }
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return transactions;
-
+        
     }
     
-    private Transaction mapRow(ResultSet row) throws SQLException
-    {
+    // monthToDate()
+    public List<Transaction> monthToDate() {
+        List<Transaction> filteredTransactions = new ArrayList<>();
+        
+        String sql = "SELECT * FROM transactions " +
+                " WHERE (EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE)) " +
+                "   AND (EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP)) ";
+        
+        try(
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet row = ps.executeQuery();
+        ) {
+            while(row.next()) {
+                Transaction transaction = mapRow(row);
+                
+                filteredTransactions.add(transaction);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return filteredTransactions;
+    }
+    
+    // previousMonth()
+    // yearToDate()
+    // previousYear()
+    // searchByVendor()
+    
+    private Transaction mapRow(ResultSet row) throws SQLException {
         int    id          = row.getInt("transaction_id");
         String date        = row.getTimestamp("date").toString();
         String description = row.getString("description");
