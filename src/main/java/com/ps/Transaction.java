ef6d1d2c4c6e81d796b81f53b5eae75e4a27e488
Marshall <mellisgibbs@gmail.com>
2024-07-16 18:02:21 -0400

    updated 'toString()'

--- src/main/java/com/ps/Transaction.java
+++ src/main/java/com/ps/Transaction.java
@@ -79,13 +79,20 @@ public class Transaction {
     @Override
     public String toString()
     {
-        return "Transaction{" +
-                "id=" + id +
-                ", date='" + date + '\'' +
-                ", description='" + description + '\'' +
-                ", vendor='" + vendor + '\'' +
-                ", amount=" + amount +
-                '}';
+//        return "Transaction{" +
+//                "id=" + id +
+//                ", date='" + date + '\'' +
+//                ", description='" + description + '\'' +
+//                ", vendor='" + vendor + '\'' +
+//                ", amount=" + amount +
+//                '}';
+        
+        if (amount == Math.abs(amount)) {
+            return String.format("~ %-12s %-30s %-20s $%-10.2f", date, description, vendor, amount);
+        } else if (amount != Math.abs(amount)) {
+            return String.format("~ %-12s %-30s %-20s -$%-10.2f", date, description, vendor, Math.abs(amount));
+        }
+        return null;
     }
 }
 
